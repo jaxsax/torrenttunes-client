76c803d7da1a2fbe639ad52412d45378e7f2bf52
alphazone <shengweichow@gmail.com>
2016-07-16 12:58:35 +0800

    core/fix: Fix argument parsing
    Arguments were not properly being passed to the Application which was causing arguments to be ignored

--- src/main/java/com/torrenttunes/client/Main.java
+++ src/main/java/com/torrenttunes/client/Main.java
@@ -3,31 +3,23 @@ package com.torrenttunes.client;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
-import java.util.Optional;
-
 public class Main {
     public static void main(String[] args) {
         Arguments arguments = new Arguments();
         CmdLineParser parser = new CmdLineParser(arguments);
-        Optional<Arguments> parsedArguments = parse(parser, args);
 
-        if (!parsedArguments.isPresent()) {
-            parser.printUsage(System.err);
-            System.exit(-1);
-        }
-        new Application().main(parsedArguments.get());
+        parse(parser, args);
+
+        new Application().main(arguments);
     }
 
-    private static Optional<Arguments> parse(CmdLineParser parser,
-                                             String[] args) {
-        Arguments arguments = new Arguments();
-
+    private static void parse(CmdLineParser parser,
+                              String[] args) {
         try {
             parser.parseArgument(args);
-            return Optional.of(arguments);
-
         } catch (CmdLineException e) {
-            return Optional.empty();
+            parser.printUsage(System.err);
+            System.exit(-1);
         }
     }
 }
