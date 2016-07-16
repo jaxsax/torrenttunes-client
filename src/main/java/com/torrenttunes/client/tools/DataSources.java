09f5874563c2e7af2ea30cfb2a75ac23d28bbd3b
alphazone <shengweichow@gmail.com>
2016-07-16 13:24:07 +0800

    core/refactor: Remove warnings in DataSources

--- src/main/java/com/torrenttunes/client/tools/DataSources.java
+++ src/main/java/com/torrenttunes/client/tools/DataSources.java
@@ -39,58 +39,50 @@ public class DataSources {
 
     public static final File SAMPLE_TORRENT = new File("/home/tyler/Downloads/[kat.cr]devious.maids.s03e01.hdtv.x264.asap.ettv.torrent");
 
-    // The path to the home dir
     public static String HOME_DIR() {
-        String userHome = System.getProperty("user.home") + "/." + APP_NAME;
-        return userHome;
+        return System.getProperty("user.home") + "/." + APP_NAME;
     }
 
-    public static final String TORRENTS_DIR() {
+    public static String TORRENTS_DIR() {
         return HOME_DIR() + "/torrents";
     }
 
-    public static final String DEFAULT_MUSIC_STORAGE_PATH() {
+    public static String DEFAULT_MUSIC_STORAGE_PATH() {
         return HOME_DIR() + "/music";
     }
 
     public static String MUSIC_STORAGE_PATH = DEFAULT_MUSIC_STORAGE_PATH();
 
-    public static final String AUDIO_FILE(String fileName) {
+    public static String AUDIO_FILE(String fileName) {
         return MUSIC_STORAGE_PATH + "/" + fileName;
-    }
-
-    public static final String SAMPLE_TORRENT_FILE() {
-        return TORRENTS_DIR() +
-                "/[kat.cr]fugazi.studio.discography.1989.2001.flac.torrent";
     }
 
     public static final String SAMPLE_MUSIC_DIR = "/home/tyler/Downloads";
 
     public static final String SAMPLE_SONG = SAMPLE_MUSIC_DIR + "/04 One Evening.mp3";
 
-    public static final String DB_FILE() {
+    public static String DB_FILE() {
         return HOME_DIR() + "/db/db.sqlite";
     }
-
 
     // This should not be used, other than for unzipping to the home dir
     public static final String CODE_DIR = System.getProperty("user.dir");
 
-    public static final String SOURCE_CODE_HOME() {
+    public static String SOURCE_CODE_HOME() {
         return HOME_DIR() + "/src";
     }
 
-    public static final String SQL_FILE() {
+    public static String SQL_FILE() {
         return SOURCE_CODE_HOME() + "/ddl.sql";
     }
 
-    public static final String SQL_VIEWS_FILE() {
+    public static String SQL_VIEWS_FILE() {
         return SOURCE_CODE_HOME() + "/views.sql";
     }
 
     public static final String SHADED_JAR_FILE = CODE_DIR + "/target/" + APP_NAME + ".jar";
 
-    public static final String SHADED_JAR_FILE_2() {
+    public static String SHADED_JAR_FILE_2() {
         try {
             String path = DataSources.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
             if (System.getProperty("os.name").contains("indow")) {
@@ -99,94 +91,76 @@ public class DataSources {
 
             return path;
         } catch (URISyntaxException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            log.error("Invalid URI", e);
         }
         return null;
     }
 
-    public static final String ZIP_FILE() {
+    public static String ZIP_FILE() {
         return HOME_DIR() + "/" + APP_NAME + ".zip";
     }
 
-    public static final String JAR_FILE() {
+    public static String JAR_FILE() {
         return HOME_DIR() + "/" + APP_NAME + ".jar";
     }
 
-    public static final String LAUNCHER_FILE() {
+    public static String LAUNCHER_FILE() {
         return HOME_DIR() + "/" + LAUNCHER_NAME + ".jar";
     }
 
-    public static final String TOOLS_JS() {
+    public static String TOOLS_JS() {
         return SOURCE_CODE_HOME() + "/web/js/tools.js";
     }
 
     // Web pages
-    public static final String WEB_HOME() {
+    public static String WEB_HOME() {
         return SOURCE_CODE_HOME() + "/web";
     }
 
-    public static final String WEB_HTML() {
+    public static String WEB_HTML() {
         return WEB_HOME() + "/html";
     }
 
-    public static final String WEB_SERVICE_STARTED_URL() {
-        return WEB_SERVICE_URL + "hello";
-    }
-
-    public static final String MAIN_PAGE_URL_EN() {
+    public static String MAIN_PAGE_URL_EN() {
         return WEB_HTML() + "/main_en.html";
     }
 
-    public static final String MAIN_PAGE_URL_ES() {
+    public static String MAIN_PAGE_URL_ES() {
         return WEB_HTML() + "/main_es.html";
     }
 
-    public static final String MAIN_PAGE_URL_FR() {
+    public static String MAIN_PAGE_URL_FR() {
         return WEB_HTML() + "/main_fr.html";
-    }
-
-
-    public static final String MAINTENANCE_PAGE_URL() {
-        return WEB_HTML() + "/maintenance.html";
     }
 
     public static String BASE_ENDPOINT = MAIN_PAGE_URL_EN();
 
-    public static final Set<String> NON_STREAMING_BROWSERS = new HashSet<String>(Arrays.asList(
-            "Firefox", "Android"));
+    public static final Set<String> NON_STREAMING_BROWSERS = new HashSet<>(
+            Arrays.asList("Firefox", "Android")
+    );
 
-    public static final String SESSION_STATS_FILE() {
+    public static String SESSION_STATS_FILE() {
         return CODE_DIR + "/session_stats.0000.log";
     }
-
-
-    public static final String PAGES(String pageName) {
-        return WEB_HTML() + "/" + pageName + ".html";
-    }
-
 
     public static final String TORRENT_UPLOAD_URL = TORRENTTUNES_URL + "torrent_upload";
 
     public static final String TORRENT_INFO_UPLOAD_URL = TORRENTTUNES_URL + "torrent_info_upload";
 
-    public static final String TORRENT_DOWNLOAD_URL(String infoHash) {
+    public static String TORRENT_DOWNLOAD_URL(String infoHash) {
         return TORRENTTUNES_URL + "download_torrent/" + infoHash;
     }
 
-    public static final String TORRENT_INFO_DOWNLOAD_URL(String infoHash) {
+    public static String TORRENT_INFO_DOWNLOAD_URL(String infoHash) {
         return TORRENTTUNES_URL + "download_torrent_info/" + infoHash;
     }
 
-    public static final String TRACKER_ANNOUNCE = "http://" + TORRENTTUNES_IP + ":6969/announce";
-
-
-    public static final String SEEDER_INFO_UPLOAD(String infoHash, String seeders) {
+    public static String SEEDER_INFO_UPLOAD(String infoHash, String seeders) {
         return TORRENTTUNES_URL + "seeder_upload/" + infoHash + "/" + seeders;
     }
 
 
-    public static final String LIBTORRENT_OS_LIBRARY_PATH() {
+    public static String LIBTORRENT_OS_LIBRARY_PATH() {
         String osName = System.getProperty("os.name").toLowerCase();
         String jvmBits = System.getProperty("sun.arch.data.model");
         log.info("Operating system: " + osName + ", JVM bits: " + jvmBits);
@@ -212,43 +186,22 @@ public class DataSources {
         return ret;
     }
 
-    public static final List<URI> ANNOUNCE_LIST() {
-        List<URI> list = null;
+    public static List<URI> ANNOUNCE_LIST() {
+        List<URI> list = new ArrayList<>();
         try {
-            list = Arrays.asList(
-                    //				new URI(TRACKER_ANNOUNCE),
-                    //				new URI("http://9.rarbg.com:2710/announce"),
-                    //				new URI("http://announce.torrentsmd.com:6969/announce"),
-                    //				new URI("http://bt.careland.com.cn:6969/announce"),
-                    //				new URI("http://explodie.org:6969/announce"),
-                    //				new URI("http://mgtracker.org:2710/announce"),
-                    //				new URI("http://tracker.best-torrents.net:6969/announce"),
-                    //				new URI("http://tracker.tfile.me/announce"),
-                    //				new URI("http://tracker.torrenty.org:6969/announce"),
-                    //				new URI("http://tracker1.wasabii.com.tw:6969/announce"),
-                    //				new URI("udp://9.rarbg.com:2710/announce"),
-                    //				new URI("udp://9.rarbg.me:2710/announce"),
-                    //				new URI("udp://coppersurfer.tk:6969/announce"),
-                    //				new URI("udp://exodus.desync.com:6969/announce"),
-                    //					new URI("udp://open.demonii.com:1337/announce"));
-                    //				new URI("udp://tracker.btzoo.eu:80/announce"),
-//									new URI("udp://tracker.istole.it:80/announce"));
+            list = Collections.singletonList(
                     new URI("udp://tracker.opentrackr.org:1337/announce"));
-//					new URI("udp://tracker.openbittorrent.com:80/announce"));
-
-            //				new URI("udp://tracker.prq.to/announce"),
-            //				new URI("udp://tracker.publicbt.com:80/announce"));
         } catch (URISyntaxException e) {
+            log.error("Invalid URI", e);
         }
 
         return list;
     }
 
-    public static final List<AnnounceEntry> ANNOUNCE_ENTRIES() {
-        return Arrays.asList(
+    public static List<AnnounceEntry> ANNOUNCE_ENTRIES() {
+        return Collections.singletonList(
                 new AnnounceEntry("udp://tracker.opentrackr.org:1337/announce")
         );
-
     }
 
     public static String WINDOWS_SHORTCUT_LINK() {
@@ -283,12 +236,8 @@ public class DataSources {
         return System.getProperty("user.home") + "/Applications/TorrentTunes.app";
     }
 
-    public static final String FETCH_LATEST_RELEASE_URL() {
+    public static String FETCH_LATEST_RELEASE_URL() {
         return "https://github.com/tchoulihan/torrenttunes-client/releases/latest";
-    }
-
-    public static String TEMP_JAR_PATH() {
-        return System.getProperty("user.home") + "/" + APP_NAME + ".jar";
     }
 
     public static String INSTALLED_VERSION_FILE() {
@@ -297,19 +246,19 @@ public class DataSources {
 
     public static final Date APP_START_DATE = new Date();
 
-    public static final String HTML_TEMPLATE_LOCATION() {
+    public static String HTML_TEMPLATE_LOCATION() {
         return WEB_HTML() + "/main.template";
     }
 
-    public static final String STRINGS_EN_LOCATION() {
+    public static String STRINGS_EN_LOCATION() {
         return SOURCE_CODE_HOME() + "/values/strings_en.json";
     }
 
-    public static final String STRINGS_ES_LOCATION() {
+    public static String STRINGS_ES_LOCATION() {
         return SOURCE_CODE_HOME() + "/values/strings_es.json";
     }
 
-    public static final String STRINGS_FR_LOCATION() {
+    public static String STRINGS_FR_LOCATION() {
         return SOURCE_CODE_HOME() + "/values/strings_fr.json";
     }
 
